9a7144dce654b9b09182541941962d0d9b4b80a5
Abdul Moiz <138908943+codewithmoiz@users.noreply.github.com>
2024-01-16 22:45:13 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+var btn = document.getElementById("button");
+var see_button = document.getElementById("see-button");
+var inputField = document.getElementById("user-input");
+var blank = "";
+var showPara;
+
+function func() {
+    var getText = inputField.value;
+
+    if (getText !== "") {
+        blank = getText;
+        if(blank.length > 50){
+            see_button.style.display = 'inline';
+        }
+        else{
+            see_button.style.display = 'none';
+        }
+        var copiedText = blank.slice(0, 50);
+        
+        // Check if the text exceeds 50 characters
+        if (blank.length > 50) {
+            // Set the copied text initially
+            showPara = document.createElement('p');
+            showPara.innerText = copiedText;
+            document.body.appendChild(showPara);
+        } else {
+            // Set the full text if it's 50 characters or less
+            showPara = document.createElement('p');
+            showPara.innerText = blank;
+            document.body.appendChild(showPara);
+        }
+    }
+}
+
+function seeText() {
+    if (see_button.innerText === "See more") {
+        // Display the full text
+        showPara.innerText = blank;
+        see_button.innerText = "See less";
+    } else {
+        // Display the copied text
+        showPara.innerText = blank.slice(0, 50);
+        see_button.innerText = "See more";
+    }
+}
